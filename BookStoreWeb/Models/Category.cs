93c1e830ea6eb31c1073381e9a9aa078e0cd6d75
najmoaden <najmoaden138@gmail.com>
2022-08-28 14:37:57 +0300

    Create Category Model

---
+++ BookStoreWeb/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStoreWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.UtcNow;
+
+    }
+}

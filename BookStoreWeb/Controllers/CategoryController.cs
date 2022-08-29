d8077042dd37db31b6d29b7ba98f4e85436e0460
najmoaden <najmoaden138@gmail.com>
2022-08-29 09:03:35 +0300

    Retrieve all Categories

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStoreWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookStoreWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;  
+        }
         public IActionResult Index()
         {
+            var objCategoryList= _db.Categories.ToList();
             return View();
         }
     }

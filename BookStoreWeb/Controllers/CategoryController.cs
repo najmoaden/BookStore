aa1ea2d42c44650b38733d034e875eef1211276e
najmoaden <najmoaden138@gmail.com>
2022-08-29 14:39:56 +0300

    Server-side Validation

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -27,9 +27,13 @@ namespace BookStoreWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
+            if (ModelState.IsValid)
+            { 
             _db.Categories.Add(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }

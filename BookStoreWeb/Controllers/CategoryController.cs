d20461de54d4ad9286d4cf078394fc2dbcf45690
najmoaden <najmoaden138@gmail.com>
2022-08-29 15:53:43 +0300

    Edit Category - GET

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -40,5 +40,39 @@ namespace BookStoreWeb.Controllers
             }
             return View(obj);
         }
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //3 ways to retrieve a category from DB
+            var categoryFromDb=_db.Categories.Find(id);
+            //var CategoryFromDbFirst = _db.Categories.FirstOrDefault(x => x.Id == id);
+            //var CategoryFromDbSingle = _db.Categories.SingleOrDefault(x => x.Id == id);
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
